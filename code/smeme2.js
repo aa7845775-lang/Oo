4ae768dbfa2fbabc6029b1bfe563fb5d7216374f
aa7845775-lang <aa7845775@gmail.com>
2025-10-22 16:51:36 +0700

    Delete code directory

--- code/smeme2.js
+++
@@ -1,9 +0,0 @@
-const axios = require("axios");
-const fs = require("fs");
-const { ImageUploadService } = require("node-upload-images");
-
-const handler = async (m, sock, { args, example, isRegis, reply, mess }) => {
-  try {
-    if (!isRegis) return reply(mess.regis);
-    const q = m.quoted ? m.quoted : m;
-    const mime = (q.msg
